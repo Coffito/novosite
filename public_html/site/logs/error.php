bc95c65141867a9e584f490a20e280a93ff30b04
Vinicius Ramos <vinicius@coffito.org.br>
2014-08-13 17:15:10 -0300

    sincronizando estrutura de arquivos conforme hospedagem do site na locaweb

---
+++ public_html/site/logs/error.php
@@ -0,0 +1,68 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-04-11 13:40:28 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-04-11T13:40:28+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-11T13:40:39+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-11T13:40:48+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-11T13:44:31+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-11T19:42:56+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-11T19:43:20+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-11T20:01:41+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-13T13:21:22+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-13T13:21:45+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-13T13:22:02+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-13T13:22:08+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-13T13:22:19+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-13T13:22:30+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-13T13:22:39+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-13T13:22:50+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-13T13:22:59+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-13T13:23:19+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-13T13:23:53+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-13T13:24:03+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-14T18:56:09+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-14T18:56:18+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-15T16:04:55+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-15T16:05:20+00:00	INFO	joomlafailure	Usuário não existe
+2014-04-15T16:06:05+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-15T17:17:17+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-16T17:07:29+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-16T17:07:57+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-16T17:08:15+00:00	INFO	joomlafailure	Usuário não existe
+2014-04-24T16:11:53+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-04-24T16:12:10+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-05-08T18:09:00+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-05-08T18:09:10+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-05-09T14:14:18+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-05-13T12:43:45+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-05-13T12:44:23+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-05-13T12:44:50+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-05-15T17:26:44+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-05-22T15:01:11+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-05-29T18:42:04+00:00	INFO	cookiefailure	Usuário não existe
+2014-05-30T16:33:34+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-06-05T16:23:01+00:00	INFO	cookiefailure	Senha inválida
+2014-06-05T16:23:24+00:00	INFO	cookiefailure	Senha inválida
+2014-06-05T17:08:42+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-06-10T14:20:08+00:00	INFO	cookiefailure	Senha inválida
+2014-06-11T16:06:19+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-06-17T14:32:51+00:00	INFO	cookiefailure	Senha inválida
+2014-06-18T17:14:54+00:00	INFO	joomlafailure	Senha inválida
+2014-06-25T14:53:04+00:00	INFO	joomlafailure	Senha inválida
+2014-06-25T17:16:37+00:00	INFO	joomlafailure	Senha inválida
+2014-06-25T18:27:21+00:00	INFO	joomlafailure	Senha inválida
+2014-07-02T18:03:49+00:00	INFO	cookiefailure	Senha inválida
+2014-07-02T18:52:55+00:00	INFO	joomlafailure	Senha inválida
+2014-07-03T18:04:02+00:00	INFO	cookiefailure	Senha inválida
+2014-07-09T19:30:13+00:00	INFO	joomlafailure	Senha inválida
+2014-07-17T17:19:21+00:00	INFO	cookiefailure	Usuário não existe
+2014-07-17T17:27:22+00:00	INFO	cookiefailure	Senha inválida
+2014-07-18T16:59:05+00:00	INFO	joomlafailure	Senha inválida
+2014-07-18T16:59:13+00:00	INFO	joomlafailure	Senha inválida
+2014-07-23T18:22:06+00:00	INFO	cookiefailure	Senha inválida
+2014-08-06T17:22:10+00:00	INFO	cookiefailure	Senha inválida
+2014-08-07T12:35:38+00:00	INFO	joomlafailure	Senha inválida
+2014-08-07T13:41:58+00:00	INFO	joomlafailure	Senha inválida
